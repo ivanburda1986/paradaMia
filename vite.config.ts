dab7af99f687cb7d86919d1602983b3783100772
Ivan Burda <iburda@Ivans-MacBook-Pro.local>
2023-07-08 21:25:40 +0200

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import {VitePWA, VitePWAOptions} from 'vite-plugin-pwa';
+import manifest from './manifest.json'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType:"prompt",
+  includeAssets: ['favicon.ico', 'apple-touch-icon', 'masked-icon-svg'],
+  manifest
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
