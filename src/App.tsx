e073e9da6974a3523b7f2e4c87181667d9c6f242
Ivan Burda <iburda@Ivans-MacBook-Pro.local>
2023-07-22 15:40:23 +0200

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 import './App.css';
 import {Monitor} from "./Monitor/Monitor.tsx";
+let deferredPrompt;
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker
+      .register('/sw.js')
+      .then(function(){
+        console.log('Service worker registered!');
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+}
+
+window.addEventListener('beforeinstallprompt', function (event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
 
 function App() {
   return (<Monitor/>)
