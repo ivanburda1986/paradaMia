ace75c51601fd5617eb76daec6563795ed73ac68
ivanburda1986 <burda.ivan@gmail.com>
2023-07-26 00:49:42 +0200

    onclick

--- src/Monitor/Monitor.tsx
+++ src/Monitor/Monitor.tsx
@@ -15,7 +15,7 @@ export const Monitor: FC = () => {
 
     return (
         <div>
-            <button className={`${styles.stopShield} py-1`} onClick={handleMonitorClick}>Absberggasse</button>
+            <button className={`${styles.stopShield} py-1`} onClick={()=>handleMonitorClick()}>Absberggasse</button>
             <div className={`${styles.monitor}`}>
                 <section className={`${styles.headers} d-flex justify-content-between align-items-center px-1`}><p>Linie</p><p>Abfahrt in Minuten</p></section>
                 {monitors?.connections.map((connection)=><MonitorConnection key={`${connection.name}-${connection.nextCounter}`} connection={connection}/>)}
