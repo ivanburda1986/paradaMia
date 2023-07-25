84ad8e0d0e30cadfc7e3e25338d7ca42170ee83f
Ivan Burda <iburda@Ivans-MacBook-Pro.local>
2023-07-25 23:11:49 +0200

    refresh monitors

--- src/Monitor/Monitor.tsx
+++ src/Monitor/Monitor.tsx
@@ -1,16 +1,22 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import {useConnections} from "./useConnections.ts";
 import {MonitorConnection} from "./MonitorConnection/MonitorConnection.tsx";
 import styles from './Monitor.module.css'
+
 export const Monitor: FC = () => {
     const {stopMonitors} = useConnections(3431);
+    const [monitors, setMonitors] = useState(stopMonitors);
+
+    const handleMonitorClick = () => {
+        setMonitors(stopMonitors)
+    };
 
     return (
         <div>
-            <div className={`${styles.stopShield} py-1`}>Absberggasse</div>
+            <button className={`${styles.stopShield} py-1`} onClick={handleMonitorClick}>Absberggasse</button>
             <div className={`${styles.monitor}`}>
                 <section className={`${styles.headers} d-flex justify-content-between align-items-center px-1`}><p>Linie</p><p>Abfahrt in Minuten</p></section>
-                {stopMonitors?.connections.map((connection)=><MonitorConnection key={`${connection.name}-${connection.nextCounter}`} connection={connection}/>)}
+                {monitors?.connections.map((connection)=><MonitorConnection key={`${connection.name}-${connection.nextCounter}`} connection={connection}/>)}
             </div>
         </div>
     );
